dbe8785de4f35b686a1b6d9cdeb637cfbe3c0f7b
Mateus <amaral5x@hotmail.com>
2023-01-24 13:23:52 -0300

    Estudando

---
+++ fundamentos/hooks/src/components/State.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react'
+
+const State = () => {
+    let cont = 0
+    const nome = 'Utilizando uma variável padrão sem alteração de estado'
+    const [contador, setContador] = useState(0)
+
+    return(
+        <div>
+            <h2>useState</h2>
+            <div>
+                <h3>Sem Estado</h3>
+                {nome}
+                <br></br>
+                {cont}
+                <br></br>
+                <button onClick={() => cont += 1}>Contador</button>
+            </div>
+
+            <div>
+                <h3>Com Estado</h3>
+                {contador}
+                <br></br>
+                <button onClick={() => setContador(contador + 1)}>Adicionar</button>
+            </div>
+        </div>
+    )
+}
+
+export default State
