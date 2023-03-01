0dd852e4ed7698da76918232c558ac6a9d5e5219
Mateus <amaral5x@hotmail.com>
2023-02-28 22:05:08 -0300

    Star Wars pt1

--- aplicacao/harrypotter/src/templates/header.template/Header.js
+++ aplicacao/harrypotter/src/templates/header.template/Header.js
@@ -1,13 +1,37 @@
 import './index.css'
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useContext, useState } from 'react'
+import { NavLink, useNavigate } from 'react-router-dom'
+
+// CONTEXTS
+import PageContext from '../../contexts/PageContext'
 
 const Header = () => {
+  const [pesquisar, setPesquisar] = useState()
+  const navigate = useNavigate()
+
+  // useContext
+  const {page} = useContext(PageContext)
+
+  const search = () => {
+    if(pesquisar.trim()){
+      navigate(`/search/${pesquisar}`)
+      setPesquisar('')
+    }
+  }
+
   return (
     <header>
         <img src="https://logosmarcas.net/wp-content/uploads/2020/04/Harry-Potter-Logo.png" alt="logo hp" />
-
+        
+        <div className="search">
+          <input type="search" 
+          onChange={(e) => setPesquisar(e.target.value)} 
+          onKeyDown={(e) => e.key === 'Enter' ? search() : null}
+          value={pesquisar} />
+          <button onClick={search}>🔍</button>
+        </div>
         <nav>
+            <span>Local /{page}</span>
             <NavLink to="/">Home</NavLink>
             <NavLink to="/characters">Characters</NavLink>
             <NavLink to="/spells">Spells</NavLink>
